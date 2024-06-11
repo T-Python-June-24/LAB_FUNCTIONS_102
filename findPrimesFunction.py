cb33e6e2980d502ccf84eb697f00bdc3a446483d
D. F <ifonei@hotmail.com>
2024-06-11 13:47:18 +0300

    adding documentations to the function.

--- findPrimesFunction.py
+++ findPrimesFunction.py
@@ -1,4 +1,13 @@
 def find_primes(start:int, end:int):
+    """"
+    It prints all prime numbers between any two given integer numbers.
+    Args:
+        start (int): The start number
+        end (int): The end number
+        
+    Return:
+        None
+    """
     for number in range(start, end+1):
         count = 0
         for i in range(2,number):
--- upperToLowerFunction.py
+++ upperToLowerFunction.py
@@ -1,4 +1,14 @@
 def lowerTheUpper(word:str):
+    """"
+    It takes a string as a parameter then checks if it's indeed of string type.
+    It separates the string at any capital letter and replace it with a small letter 
+    then return the new word.
+    Args:
+        word (str): The string to be separated
+        
+    Return:
+        newWord: a new string where all upper letters are replace with lower ones.
+    """
     newWord = ""
     if word.isalpha():
         for i in word:
