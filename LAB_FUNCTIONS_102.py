cab78c3278c7829bc2b615adf96b626d1a7e398d
naif <naif.n1158@gmail.com>
2024-06-11 12:57:58 +0300

    Solved

--- LAB_FUNCTIONS_102.py
+++ LAB_FUNCTIONS_102.py
@@ -1,5 +1,11 @@
 def find_primes(num1:int, num2:int):
-    for i in range(num1 , num2):
+    '''A function in which only odd numbers are displayed
+    Args:
+        num1 (int): First issue
+        num2 (int): The second issue
+    
+    '''
+    for i in range(num1 , num2 ):
         if i % 2 ==1:
             print(i)
         
--- bonus.py
+++ bonus.py
@@ -1,4 +1,10 @@
 def string(text:str):
+    '''A function that edits text, converts uppercase letters to lowercase, and places a space before the letter
+    Args:
+        (string): The text to be modified
+    return:
+    text1 (str): Text after modification
+    '''
     text1:str= ""
     if type(text)== str:
         for i in text:
