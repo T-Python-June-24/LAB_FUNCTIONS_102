0675b05258623e04b9f06dd464ed9ff36ffd872c
AZ-007 <aboodi-75-@hotmail.com>
2024-06-11 14:21:52 +0300

    write a documentation

--- Program.py
+++ Program.py
@@ -1,5 +1,5 @@
 def find_primes(num1:int, num2:int):
-    
+    ''' This function finds prime numbers'''
     for num in range(num1, num2 + 1):
         is_prime = True
         for i in range(2, int(num ** 0.5) + 1):
@@ -10,3 +10,4 @@ def find_primes(num1:int, num2:int):
             print(num)
 
 find_primes(25, 50)
+help(find_primes)
