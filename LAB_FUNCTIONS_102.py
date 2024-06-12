473c96c426e12ccc1891ba4f35e8bee104081608
naif <naif.n1158@gmail.com>
2024-06-12 19:50:51 +0300

    done edit

--- LAB_FUNCTIONS_102.py
+++ LAB_FUNCTIONS_102.py
@@ -1,13 +1,18 @@
-def find_primes(num1:int, num2:int):
-    '''A function in which only odd numbers are displayed
+def find_primes(num1: int, num2: int):
+    '''A function to display prime numbers between two given numbers.
     Args:
-        num1 (int): First issue
-        num2 (int): The second issue
-    
+        num1 (int): First number.
+        num2 (int): Second number.
     '''
-    for i in range(num1 , num2 ):
-        if i % 2 ==1:
-            print(i)
-        
-    
-find_primes(25 , 50)
+    for i in range(num1, num2 + 1):
+        if i > 1:  
+            is_prime = True
+            for j in range(2, int(i**0.5) + 1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                print(i)
+
+
+find_primes(25, 50)
